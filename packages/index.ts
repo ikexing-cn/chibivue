31de0f1341e9d36e63be26ef913e049a2fac6bf2
ikexing-cn <ikexing@qq.com>
2023-11-28 14:35:11 +0800

    feat: create app

--- packages/index.ts
+++ packages/index.ts
@@ -1,3 +1,18 @@
-export function helloChibivue() {
-  console.log('Hello Chibivue!')
+export type Options = {
+  render: () => string
+}
+
+export type App = {
+  mount: (selector: string) => void
+}
+
+export const createApp = (options: Options): App => {
+  return {
+    mount: (selector) => {
+      const root = document.querySelector(selector)
+      if (root) {
+        root.innerHTML = options.render()
+      }
+    },
+  }
 }
