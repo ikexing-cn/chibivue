0600d8187a155f8f295e9e8e7ad8979f2cb415e7
ikexing-cn <ikexing@qq.com>
2023-11-28 15:19:17 +0800

    feat: h

--- packages/runtime-core/index.ts
+++ packages/runtime-core/index.ts
@@ -1,2 +1,3 @@
 export * from './apiCreateApp'
 export * from './renderer'
+export * from './h'
--- packages/runtime-dom/nodeOps.ts
+++ packages/runtime-dom/nodeOps.ts
@@ -1,7 +1,16 @@
 import type { RendererOptions } from '../runtime-core'
 
 export const nodeOps: RendererOptions<Node> = {
+  createElement(type) {
+    return document.createElement(type)
+  },
+  createText(text) {
+    return document.createTextNode(text)
+  },
   setElementText(node, text) {
     node.textContent = text
+  },
+  insert(child, parent, anchor) {
+    parent.insertBefore(child, anchor || null)
   },
 }
